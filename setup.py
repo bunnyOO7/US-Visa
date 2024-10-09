eb8623fdccccc48278b9615966b9a7730b515243
Aditya Bhardwaj <adityabhardwaj792@gmail.com>
2024-10-09 14:35:35 +0530

    day 2

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_VISA",
+    version="0.0.0",
+    author="Aditya",
+    author_email="adityabhardwaj792@gmail.com",
+    packages=find_packages()
+)
